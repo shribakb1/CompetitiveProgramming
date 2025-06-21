ffa27814072880e4f99b0ea1fd63dfdef86ed013
Bohdan Shribak <shribakb@gmail.com>
2025-06-21 19:23:58 +0300

    add solution for B problem

--- Codeforces Round 1032 (Div. 3)/Solution/Solution/B.cpp
+++ Codeforces Round 1032 (Div. 3)/Solution/Solution/B.cpp
@@ -8,10 +8,24 @@ int main()
 	{
 		int n; 
 		std::cin >> n;
-		
+
 		std::string str;
 		std::cin >> str;
+		
+		for (int i = 1; i < n - 1; i++)
+		{
+			std::string a = str.substr(0, i);
+			std::string c = str.substr(i + 1, n - i - 1);
 
+			if (a.find(str[i]) != std::string::npos || c.find(str[i]) != std::string::npos)
+			{
+				std::cout << "Yes" << std::endl;
+				break;
+			}
+			if (i + 2 == n)
+			{
+				std::cout << "No" << std::endl;
+			}
+		}
 	}
-	
 }
