a1bdb22e36a5473e9f9c7092c625e1211c0f0fe7
Bohdan Shribak <shribakb@gmail.com>
2025-06-22 17:09:28 +0300

    some things

---
+++ Codeforces Round 1032 (Div. 3)/Solution/Solution/D.cpp
@@ -0,0 +1,33 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.06.21 20:04:56
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        vector<int> a(n), b(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+
+        
+    }
+
+    return 0;
+}
---
+++ Codeforces Round 479 (Div. 3)/A.cpp
@@ -0,0 +1,31 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.06.22 16:35:34
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (n % 10 == 0)
+        {
+            n  /= 10;
+        }
+        else
+        {
+            n -= 1;
+        }
+    }
+
+    cout << n;
+
+    return 0;
+}
