a14842547f47c653636fa06016d57b54bad987af
Bohdan Shribak <shribakb@gmail.com>
2025-06-30 02:31:43 +0300

    add solution for A problem

---
+++ Codeforces Round 253 (Div. 2)/A.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.06.30 02:23:51
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    set<char> ch;
+
+    string str;
+    getline(cin, str);    
+
+    
+    cout << ch.size();
+    return 0;
+}
