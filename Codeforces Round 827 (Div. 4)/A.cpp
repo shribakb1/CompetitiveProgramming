4e94aa4bd32f6ed0d645fc15872ab225ad5df876
Bohdan Shribak <shribakb@gmail.com>
2025-07-01 16:56:02 +0300

    add solution for A problem

---
+++ Codeforces Round 827 (Div. 4)/A.cpp
@@ -0,0 +1,22 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.07.01 16:53:32
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+    }
+    return 0;
+}
