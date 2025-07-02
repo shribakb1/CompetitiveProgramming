e84e5e52dadf14ec324f3d8a2f4837faba0c1693
Bohdan Shribak <shribakb@gmail.com>
2025-07-03 01:42:58 +0300

    add solution for A problem

---
+++ Experimental Educational Round VolBIT Formulas Blitz/A.cpp
@@ -0,0 +1,25 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.07.03 01:38:53
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    int n;
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << 5;
+    }
+    else
+    {
+        cout << 25;
+    }
+    return 0;
+}
