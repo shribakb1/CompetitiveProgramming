968f31dba92192718d4424b4bd8e8c649fac628d
Bohdan Shribak <shribakb@gmail.com>
2025-07-08 21:22:18 +0300

    add solution for B problem

---
+++ Codeforces Round 628 (Div. 2)/B.cpp
@@ -0,0 +1,33 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.07.08 21:20:24
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        set<int> a;
+
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+
+            a.insert(x);
+        }
+
+        cout << a.size() << endl;
+    }
+
+    return 0;
+}
