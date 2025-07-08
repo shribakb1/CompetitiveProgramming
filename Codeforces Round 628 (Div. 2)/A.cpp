803ee3464ff4ca8d77abf3d01a446677dddec34d
Bohdan Shribak <shribakb@gmail.com>
2025-07-08 21:20:13 +0300

    add solution for A problem

---
+++ Codeforces Round 628 (Div. 2)/A.cpp
@@ -0,0 +1,22 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.07.08 21:18:29
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << 1 << " " << n - 1 << endl;
+    }
+    return 0;
+}
