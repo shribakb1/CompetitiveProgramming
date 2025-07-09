8465b02d4897fbf79acf3f8110a4b0527e3988ef
Bohdan Shribak <shribakb@gmail.com>
2025-07-09 03:52:34 +0300

    add solution for A problem

---
+++ Codeforces Round 165 (Div. 2)/A.cpp
@@ -0,0 +1,31 @@
+/**
+ *    author:  shribakb
+ *    created: 2025.07.09 03:45:40
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a;
+        cin >> a;
+
+        if (360 % (180 - a) == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
